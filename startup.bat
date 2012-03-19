26c43b4e933fa5658db8050eae9e7f4b00bc9897
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-03-19 15:07:28 +0900

    Periodical

---
+++ startup.bat
@@ -0,0 +1,6 @@
+rem start %SystemRoot%\system32\cmd.exe -C:\WORKS\WORKSPACES\ADMIN_USB_1 --console suppress
+start %SystemRoot%\system32\cmd.exe --console suppress
+rem start %SystemRoot%\system32\cmd.exe /C cd C:\WORKS\WORKSPACES\ADMIN_USB_1
+start C:\WORKS
+start C:\"Documents and Settings"\iwabuchiken\"Local Settings"\"Application Data"\Google\Chrome\Application\chrome.exe
+start C:\"Program Files"\"NetBeans 7.1.1_CDT"\bin\netbeans.exe"
